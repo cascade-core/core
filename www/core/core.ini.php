1e42249ee79ed7c3bfb68dedd6f292a066ed57c3
Josef Kufner <jk@frozen-doe.net>
2010-03-11 01:18:45 +0100

    Basic index.php

---
+++ www/core/core.ini.php
@@ -0,0 +1,13 @@
+; <?php exit(); ?>
+; 
+; Core fallback config file
+;
+
+[php]
+; php.ini options here
+
+[module:INIT]
+module = core/init
+force-exec = true
+
+; vim:encoding=utf-8:filetype=dosini:
---
+++ www/index.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Copyright (c) 2010, Josef Kufner  <jk@myserver.cz>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* Define basic constants. Each DIR_* must be terminated by slash! */
+define('DIR_ROOT',		dirname(__FILE__).'/');
+define('DIR_CORE_CLASS',	DIR_ROOT.'core/class/');
+define('DIR_CORE_MODULE',	DIR_ROOT.'core/module/');
+define('DIR_APP_CLASS',		DIR_ROOT.'app/class/');
+define('DIR_APP_MODULE',	DIR_ROOT.'app/module/');
+define('FILE_APP_CONFIG',	DIR_ROOT.'app/core.ini.php');
+define('FILE_CORE_CONFIG',	DIR_ROOT.'core/core.ini.php');
+
+/* Class autoloader */
+function __autoload($class)
+{
+	if ($class[0] == 'M' && $class[1] == '_') {
+		$m = strtolower(str_replace('__', '/', substr($class, 2)));
+		include((strncmp($m, 'core/', 5) == 0 ? DIR_CORE_MODULE : DIR_APP_MODULE).$m.'.php');
+	} else {
+		$f = strtolower($class).'.php';
+		$cf = DIR_CORE_CLASS.$f;
+		$af = DIR_APP_CLASS.$f;
+
+		if (is_readable($cf)) {
+			include($cf);
+		} else if (is_readable($af)) {
+			include($af);
+		}
+	}
+}
+
+/* Load core configuration */
+if (is_readable(FILE_APP_CONFIG)) {
+	$core_cfg = parse_ini_file(FILE_APP_CONFIG);
+} else {
+	$core_cfg = parse_ini_file(FILE_CORE_CONFIG);
+}
+
+/* Load php.ini options */
+if (isset($core_cfg['php'])) {
+	foreach($core_cfg['php'] as $k => $v) {
+		ini_set($k, $v);
+	}
+}
+
+/* Pipeline */
+$Pipeline = new PipelineController();
+foreach ($core_cfg as $section => $opts) {
+	list($keyword, $id) = explode(':', $section, 2);
+	if ($keyword == 'module' && isset($id) && isset($opts['module'])) {
+		$Pipeline->add_module($id, $opts['module'], !empty($opts['force-exec']), $opts);
+	}
+}
+$Pipeline->start();
+
+/* Output */
+// TODO
+
+
