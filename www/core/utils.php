55b7641df7df4ed7f5b24e3b2817a31a3d18f31d
Josef Kufner <jk@frozen-doe.net>
2010-04-02 15:21:17 +0200

    Contact mail changed.

--- www/core/utils.php
+++ www/core/utils.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2010, Josef Kufner  <jk@myserver.cz>
+ * Copyright (c) 2010, Josef Kufner  <jk@frozen-doe.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
