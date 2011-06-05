a31fac78f83aa922c0e4ef890262a9bc4c0c82d2
Josef Kufner <jk@frozen-doe.net>
2011-06-06 00:26:33 +0200

    value/pipeline_multi_loader: load list of modules

---
+++ module/value/pipeline_multi_loader.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright (c) 2011, Josef Kufner  <jk@frozen-doe.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+class M_core__value__pipeline_multi_loader extends Module {
+
+	protected $inputs = array(
+		'list' => array(),		// array of names
+		'name' => null,			// format string for sprintf(name, list[i])
+	);
+
+	protected $outputs = array(
+		'*' => true,
+		'done' => true,
+	);
+
+
+	public function main()
+	{
+		$name = $this->in('name');
+
+		foreach ((array) $this->in('list') as $i) {
+			$mod = $name !== null ? sprintf($name, $i) : $i;
+			$id = preg_replace('/[^a-zA-Z0-9_]+/', '_', $i);
+			$this->pipeline_add($id, $mod, true, array(
+					'enable' => array('parent', 'done'),
+				));
+		}
+		$this->out('done', true);
+	}
+}
+
+
+
