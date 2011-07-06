b2a5385fefdb476f0d6ce03d3657765962ac596b
Josef Kufner <jk@frozen-doe.net>
2011-07-06 22:06:06 +0200

    Table: Column value can be calculated with function or set to constant

--- template/html5/table.php
+++ template/html5/table.php
@@ -87,17 +87,45 @@ class tpl_html5__core__table__text {
 
 	function th()
 	{
-		echo "<th align=\"left\">", htmlspecialchars($this->opts['title']), "</th>\n";
+		if (isset($this->opts['title_tooltip'])) {
+			$title = ' title="'.htmlspecialchars($this->opts['title_tooltip']).'"';
+		} else {
+			$title = '';
+		}
+
+		echo "<th align=\"left\"", $title, ">", nl2br(htmlspecialchars(@$this->opts['title'])), "</th>\n";
 	}
 
 	function td($row_data)
 	{
-		echo "<td>", $this->fmt_value($row_data), "</td>\n";
+		echo "<td ", !empty($this->opts['nowrap']) ? ' nowrap' : '', ">", $this->fmt_value($row_data), "</td>\n";
 	}
 
 	function fmt_value($row_data)
 	{
-		$value = $row_data[$this->opts['key']];
+		if (isset($this->opts['value'])) {
+			if (is_callable($this->opts['value'])) {
+				$value = $this->opts['value']($row_data);
+			} else {
+				$value = $this->opts['value'];
+			}
+		} else if (isset($this->opts['key'])) {
+			if (is_array($this->opts['key'])) {
+				$value = $row_data;
+				foreach ($this->opts['key'] as $k) {
+					if (isset($value[$k])) {
+						$value = $value[$k];
+					} else {
+						$value = null;
+						break;
+					}
+				}
+			} else {
+				$value = $row_data[$this->opts['key']];
+			}
+		} else {
+			$value = null;
+		}
 		$fmt = @$this->opts['format'];
 
 		if ($value === null) {
@@ -137,3 +165,21 @@ class tpl_html5__core__table__number extends tpl_html5__core__table__text {
 	}
 }
 
+class tpl_html5__core__table__checkbox extends tpl_html5__core__table__text {
+
+	function th()
+	{
+		echo "<th></th>\n";
+	}
+
+	function col()
+	{
+		echo "<col width=\"1\">\n";
+	}
+
+	function td($row_data)
+	{
+		echo "<td><input type=\"checkbox\" name=\"", $this->fmt_value($row_data), "\"></td>\n";
+	}
+
+}
