7ef19a49e9c046e723a0a5bc5c46d7877fa792f0
Josef Kufner <jk@frozen-doe.net>
2014-01-15 20:28:10 +0100

    Update skeleton-index.php

--- skeleton-index.php
+++ skeleton-index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2010, Josef Kufner  <jk@frozen-doe.net>
+ * Copyright (c) 2010,2014  Josef Kufner  <jk@frozen-doe.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,21 +16,28 @@
  *
  */
 
+
 /*
  * Yes, you can freely skip this file if you set your web server
  * directly to core/index.php.
  */
 
 
-/* Define directory and file names.
- * Each DIR_* must be slash-terminated!
- * These constanst will be defined (but not overwritten) in core/index.php.
+/* Define root directory -- it must be slash-terminated!
+ * This constanst will be defined (but not overwritten) in core/index.php.
  */
-//define('DIR_ROOT',		dirname(dirname(__FILE__)).'/');
-//define('DIR_CORE',		DIR_ROOT.'core/');
-//define('DIR_APP',		DIR_ROOT.'app/');
-//define('DIR_PLUGIN',		DIR_ROOT.'plugin/');
+//define('DIR_ROOT',			dirname(__FILE__).'/');
+//define('DIR_CORE',			DIR_ROOT.'core/');
+//define('DIR_APP',			DIR_ROOT.'app/');
+//define('DIR_PLUGIN',			DIR_ROOT.'plugin/');
 
-/* bootstrap core */
+
+/* Configuration loader class name
+ * This constanst will be defined (but not overwritten) in core/index.php.
+ */
+//define('CLASS_CONFIG_LOADER',		'JsonConfig');
+
+
+/* bootstrap the core */
 require(defined('DIR_CORE') ? DIR_CORE.'index.php' : './core/index.php');
 
