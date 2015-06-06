f77238e67d9a8d86c042cd330e92e03df00f2570
Josef Kufner <josef@kufner.cz>
2015-06-06 18:17:57 +0200

    Generate documentation for cascade-related libraries

--- bin/build-plugin-documentation.sh
+++ bin/build-plugin-documentation.sh
@@ -17,30 +17,48 @@
 # limitations under the License.
 #
 
-if ! pwd | grep -q '/plugin/[^/]*/doc'
+if pwd | grep -q '/plugin/[^/]*/doc'
 then
+
+	(
+		# get Doxyfile
+		sed 's|\(=[\t ]*\)doc/|\1../../core/doc/|' ../../../core/doc/Doxyfile
+
+		# override few options
+		echo "PROJECT_NAME=\"`head -n 1 ../README.md`\""
+		echo "PROJECT_BRIEF="
+		echo "PROJECT_NUMBER=\"`git describe --tag --match "v[0-9]*"`\""
+
+		## add tagfiles (link other docs)
+		# echo "TAGFILES+=\"../../core/doc/doxygen/tagfile.xml=../../../../../core/doc/doxygen/html/\""
+
+		# include stdin
+		[ "$0" == "-" ] && cat
+
+		echo Generating documentation ... it may take a few more seconds ... >&2
+
+	) | ( cd .. && doxygen - )
+
+elif pwd | grep -q '/lib/cascade/[^/]*/doc'
+then
+
+	(
+		# get Doxyfile
+		sed 's|\(=[\t ]*\)doc/|\1../../../../core/doc/|' ../../../../core/doc/Doxyfile
+
+		# override few options
+		echo "PROJECT_NAME=\"`head -n 1 ../README.md`\""
+		echo "PROJECT_BRIEF="
+		echo "PROJECT_NUMBER=\"`git describe --tag --match "v[0-9]*"`\""
+
+		# include stdin
+		[ "$0" == "-" ] && cat
+
+		echo Generating documentation ... it may take a few more seconds ... >&2
+
+	) | ( cd .. && doxygen - )
+else
 	echo "This script must be run from plugin's doc directory." >&2
 	exit 1
 fi
-
-
-
-(
-	# get Doxyfile
-	sed 's|\(=[\t ]*\)doc/|\1../../core/doc/|' ../../../core/doc/Doxyfile
-
-	# override few options
-	echo "PROJECT_NAME=\"`head -n 1 ../README.md`\""
-	echo "PROJECT_BRIEF="
-	echo "PROJECT_NUMBER=\"`git describe --tag --match "v[0-9]*"`\""
-
-	## add tagfiles (link other docs)
-	# echo "TAGFILES+=\"../../core/doc/doxygen/tagfile.xml=../../../../../core/doc/doxygen/html/\""
-
-	# include stdin
-	[ "$0" == "-" ] && cat
-
-	echo Generating documentation ... it may take a few more seconds ... >&2
-
-) | ( cd .. && doxygen - )
 
